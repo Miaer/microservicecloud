ec787a568cc274c4aaae5a354439ce39486147af
mrliz <mr.lizegan@outlook.com>
2020-12-09 00:23:56 +0800

    eureka服务端和客户端注册、启动、测试

---
+++ microservicecloud-eureka-7001/src/main/java/com/springcloud/MicroservicecloudEureka7001.java
@@ -0,0 +1,17 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author mrliz
+ * @Date 2020/12/8 16:02
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class MicroservicecloudEureka7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicecloudEureka7001.class,args);
+    }
+}
