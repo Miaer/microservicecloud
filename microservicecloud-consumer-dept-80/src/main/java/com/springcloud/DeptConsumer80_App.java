21670865f74a232f85fa2f06c10ee3c05434f6da
mrliz <mr.lizegan@outlook.com>
2020-12-10 23:25:48 +0800

    自定义负载均衡算法

--- microservicecloud-consumer-dept-80/src/main/java/com/springcloud/DeptConsumer80_App.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/springcloud/DeptConsumer80_App.java
@@ -3,6 +3,8 @@ package com.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import rules.SelfRoudomRule;
 
 /**
  * @Author mrliz
@@ -10,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "MICROSERVICECLOUD-DEPT",configuration = SelfRoudomRule.class)
 public class DeptConsumer80_App {
 
     public static void main(String[] args) {
--- microservicecloud-consumer-dept-80/src/main/java/rules/SelfRoudomRule.java
+++ microservicecloud-consumer-dept-80/src/main/java/rules/SelfRoudomRule.java
@@ -1,31 +1,20 @@
-package com.springcloud.beans;
+package rules;
 
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
 import com.netflix.loadbalancer.RetryRule;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * @Author mrliz
- * @Date 2020/12/9 21:23
+ * @Date 2020/12/10 23:19
  */
 @Configuration
-public class ConfigBean {
-
-    @Bean
-    @LoadBalanced
-    public RestTemplate getRestTemplate()
-    {
-        return new RestTemplate();
-    }
-
+public class SelfRoudomRule {
     @Bean
     public IRule myRule(){
         // AbstractLoadBalancerRule下有继承的各种算法
-//        return new RandomRule();
-        return new RetryRule();
+        return new RandomRule();
     }
 }
