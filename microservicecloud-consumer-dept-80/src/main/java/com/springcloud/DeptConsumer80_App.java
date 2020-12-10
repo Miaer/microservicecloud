688269ade434220cd228e89ea61ae294add3672b
mrliz <mr.lizegan@outlook.com>
2020-12-10 21:45:58 +0800

    添加Ribbon依赖并测试

--- microservicecloud-consumer-dept-80/src/main/java/com/springcloud/DeptConsumer80_App.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/springcloud/DeptConsumer80_App.java
@@ -2,12 +2,14 @@ package com.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author mrliz
  * @Date 2020/12/9 21:24
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptConsumer80_App {
 
     public static void main(String[] args) {
