f3b7aabbfd644259fc87be4392ebf9f6d96d309d
ze <mr.lizegan@outlook.com>
2021-01-04 14:29:55 +0800

    添加nacos配置中心

---
+++ microservicecloud-nacos-provider-8081/src/main/java/com/springcloud/config/ConfigController.java
@@ -0,0 +1,25 @@
+package com.springcloud.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description: nacos配置拉取
+ * @author: LZG
+ * @create: 2021-01-04 11:49
+ **/
+@RestController
+@RequestMapping("/config")
+@RefreshScope
+public class ConfigController {
+
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
+
+    @RequestMapping("/get")
+    public boolean get() {
+        return useLocalCache;
+    }
+}
