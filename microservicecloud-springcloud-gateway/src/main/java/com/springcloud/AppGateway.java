f9d8167cf98fad45edcca9161aed1aac4b76c2c9
mrliz <mr.lizegan@outlook.com>
2021-01-05 23:27:27 +0800

    gateway模块添加

---
+++ microservicecloud-springcloud-gateway/src/main/java/com/springcloud/AppGateway.java
@@ -0,0 +1,16 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mrliz
+ */
+@SpringBootApplication
+public class AppGateway {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppGateway.class, args);
+    }
+
+}
