6b8c636f0e66d71bdb9971adaa59f2dd213b694d
ze <mr.lizegan@outlook.com>
2021-01-18 10:45:46 +0800

    web service client

---
+++ microservicecloud-web-service-server/src/main/java/com/web/service/server/service/WebServiceClient.java
@@ -0,0 +1,13 @@
+package com.web.service.server.service;
+
+/**
+ * @description: web service client
+ * @author: LZG
+ * @create: 2021-01-18 10:39
+ **/
+public class WebServiceClient {
+
+    public static void main(String[] args) {
+
+    }
+}
